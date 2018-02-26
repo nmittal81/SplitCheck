2fe8ca541674493df9f5e09389283392f14c13d1
nmittal81 <neharohan.chopra@gmail.com>
2018-02-26 14:27:35 -0500

    Need to close the keyboard

--- SplitCheck/Split Check/CheckViewController.swift
+++ SplitCheck/Split Check/CheckViewController.swift
@@ -278,3 +278,15 @@ extension CheckViewController: PersonHeaderViewDelegate {
         }
     }
 }
+ 
+ extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+ }
